a9959a72ae66260d07299168e8fcbaf552d3e25c
trkbt10 <triplequartet@gmail.com>
2026-02-22 17:18:47 +0900

    feat: exclude uncompressed .dic.bin from vite build, keep only .deflate

---
+++ npm/micado-wasm/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+    assetsInlineLimit: 0,
+  },
+  assetsInclude: [
+    "**/*.wasm",
+    "**/*.dic.bin.deflate",
+    "**/LICENSE.dic.bin",
+  ],
+  publicDir: false,
+  plugins: [
+    {
+      name: "exclude-uncompressed-dic",
+      generateBundle(_, bundle) {
+        for (const fileName of Object.keys(bundle)) {
+          // Vite renames assets like: tiny.dic.bin -> tiny.dic-HASH.bin
+          // Keep: *.deflate, LICENSE.dic*
+          // Exclude: uncompressed dic files (e.g., tiny.dic-HASH.bin, full.dic-HASH.bin)
+          if (
+            fileName.match(/\.dic[^/]*\.bin$/) &&
+            !fileName.includes("deflate") &&
+            !fileName.includes("LICENSE")
+          ) {
+            delete bundle[fileName];
+          }
+        }
+      },
+    },
+  ],
+});
